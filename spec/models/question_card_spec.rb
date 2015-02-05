309c40fe3ab91c05c1f1f9dc4ba07fc4eafc6bf4
cfmeyers <cfmeyers@gmail.com>
2015-02-05 13:56:25 -0500

    add rspec stuff

--- spec/models/question_card_spec.rb
+++ spec/models/question_card_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe QuestionCard, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe QuestionCard do
+
+  describe "when many answer cards are created that reference it" do
+    it "will have many answer cards" do
+      user = User.create(fname:"Bob", lname:"Smith", email:"bob@bob.com", password:"123")
+      q_card = QuestionCard.new()
+
+    end
+  end
+
 end
+
+
+  # create_table "answer_cards", force: :cascade do |t|
+  #   t.string   "user_id"
+  #   t.string   "text"
+  #   t.string   "question_card_id"
+  #   t.datetime "created_at",       null: false
+  #   t.datetime "updated_at",       null: false
+  # end
+
+  # create_table "question_cards", force: :cascade do |t|
+  #   t.string   "user_id"
+  #   t.string   "text"
+  #   t.string   "url"
+  #   t.string   "expiration_date"
+  #   t.string   "datetime"
+  #   t.datetime "created_at",      null: false
+  #   t.datetime "updated_at",      null: false
+  # end
+
+  # create_table "users", force: :cascade do |t|
+  #   t.string   "fname"
+  #   t.string   "lname"
+  #   t.string   "email"
+  #   t.string   "password_digest"
+  #   t.string   "image_url"
+  #   t.datetime "created_at",      null: false
+  #   t.datetime "updated_at",      null: false
+  # end
+
+
