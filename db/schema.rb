3b00bb51a8bba428e4f36c97084d1e18939d20de
jabattack1 <peter.hahn@live.com>
2015-02-05 12:03:16 -0500

    session

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205161926) do
+ActiveRecord::Schema.define(version: 20150205165345) do
+
+  create_table "answer_cards", force: :cascade do |t|
+    t.string   "user_id"
+    t.string   "text"
+    t.string   "question_card_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "question_cards", force: :cascade do |t|
+    t.string   "user_id"
+    t.string   "text"
+    t.string   "url"
+    t.string   "expiration_date"
+    t.string   "datetime"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "fname"
