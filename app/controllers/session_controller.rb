cbbcfa35f8f56561e24dd47e43b34c12f2e83f3b
jabattack1 <peter.hahn@live.com>
2015-02-05 11:31:05 -0500

    session

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,21 @@
+class SessionController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+  	user = User.find_by(email: params[:email])
+
+  	if user && user.password_digest == params[:password]
+		redirect_to '/index'
+	else
+		render :new	
+  	end
+  end
+
+  def destroy
+  	reset_session
+  	redirect_to '/login'
+  end
+
+end
