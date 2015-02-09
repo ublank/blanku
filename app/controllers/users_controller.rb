dc6fbd92381de2fcbfd7ea9de6bfa66af9108639
reihned <reihned@gmail.com>
2015-02-09 11:52:34 -0500

    render json: response.body(), but is there a better way where i can just send the raw response?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,37 @@
 # require 'tk'
 
 class UsersController < ApplicationController
-  def index
-  end
-
-  def show
-  end
+  # def index
+  # end
+  #
+  # def show
+  # end
 
   def create
+    # this can be moved to the front end
+    # if params["password"].length <= 7
+    #   @notice = "Password must be more than 7 characters! BlankU!"
+    #
+    # else
 
-    pw = params["password"]
-    
-    if pw.length <= 7
-      @notice = "Password must be more than 7 characters! BlankU!"
-      
-    else
 		stuff = User.create({
  			fname: params["fname"],
  			lname: params["lname"],
  			email: params["email"],
  			password: params["password"],
  			image_url: params["image_url"]
- 			})
-		@user = stuff
+ 		})
 
-    	if @user.save
-      # Deliver the signup email
-      		UserNotifier.send_signup_email(@user).deliver
-      		redirect_to(@user, :notice => 'User created')
-    	else
-      		render :action => 'new'
-    	end
+    	# if @user.save
+      # # Deliver the signup email
+      # 		UserNotifier.send_signup_email(@user).deliver
+      # 		redirect_to(@user, :notice => 'User created')
+    	# else
+      # 		response
+    	# end
 
-    end
+    # end
 
-	end
+  end
 
 end
-
-
