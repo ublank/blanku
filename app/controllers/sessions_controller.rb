71adea9ab0a28034d8af178a8ab5cb2532342dd1
cfmeyers <cfmeyers@gmail.com>
2015-02-09 10:48:20 -0500

    remove binding.pry from sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,9 @@
-class SessionsController < ApplicationController
+class SessionController < ApplicationController
   # def new
   #   render :new
   # end
 
   def create 
-    binding.pry
   	user = User.find_by(email: params[:email])
 
     if user && user.authenticate(params[:password])
