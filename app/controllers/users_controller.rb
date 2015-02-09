0d9dd2bece4fc31c101db24b5030b43f4356b162
reihned <reihned@gmail.com>
2015-02-09 10:43:48 -0500

    noting some things to change up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,40 @@
 # require 'tk'
 
 class UsersController < ApplicationController
-  def index
-  end
-
-  def show
-  end
+  # def index
+  # end
+  #
+  # def show
+  # end
 
   def create
 
-    pw = params["password"]
-    
-    if pw.length <= 7
-      @notice = "Password must be more than 7 characters! BlankU!"
-      
-    else
-		stuff = User.create({
- 			fname: params["fname"],
- 			lname: params["lname"],
- 			email: params["email"],
- 			password: params["password"],
- 			image_url: params["image_url"]
- 			})
-		@user = stuff
+    # move this to the front end
 
-    	if @user.save
-      # Deliver the signup email
-      		UserNotifier.send_signup_email(@user).deliver
-      		redirect_to(@user, :notice => 'User created')
-    	else
-      		render :action => 'new'
-    	end
+    # if params["password"] <= 7
+    #   @notice = "Password must be more than 7 characters! BlankU!" # lol peter
+    #
+    # else
 
-    end
+  		stuff = User.create({
+   			fname:      params["fname"],
+   			lname:      params["lname"],
+   			email:      params["email"],
+   			password:   params["password"],
+   			image_url:  params["image_url"]
+   		})
+  		@user = stuff
 
-	end
+      if @user.save
+        # Deliver the signup email
+        UserNotifier.send_signup_email(@user).deliver
+        redirect_to(@user, :notice => 'User created')
+      else
+        render :action => 'new'
+      end
+
+    # end
+
+  end
 
 end
-
-
