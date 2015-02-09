360d68d5ed096effc024c6f6d3d839104dac78f2
reihned <reihned@gmail.com>
2015-02-09 10:54:33 -0500

    using response to send back errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,17 @@ class SessionsController < ApplicationController
   #   render :new
   # end
 
-  def create 
+  def create
     binding.pry
   	user = User.find_by(email: params[:email])
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      session[:error] = nil # "Success"
+      response.headers["Content-Type"] = "application/json"
+      response.body = JSON.generate({Login: 'Success'})
 	  else
-      session[:error] = "Login Error"
+      response.headers["Content-Type"] = "application/json"
+      response.body = JSON.generate({Login: 'Error'})
     end
   end
 
