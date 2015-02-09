4f7d2a9f0bed697e6901cadfeebd52fba4d0635c
reihned <reihned@gmail.com>
2015-02-09 12:26:27 -0500

    added error messages, and fixed formatting, need to utilize the messages in the front end

--- app/controllers/api/answer_cards_controller.rb
+++ app/controllers/api/answer_cards_controller.rb
@@ -1,47 +1,40 @@
 class Api::AnswerCardsController < ApplicationController
 
-  def index 
-    if session[:user_id]
-      question_card = QuestionCard.find(params[:question_card_id])
-      answer_cards = question_card.answer_cards
-      puts answer_cards
-      render json: answer_cards
-    else
-      render json: {errors: "you're totally not logged in"}, :status => 403
-    end
+  def index
+    question_card = QuestionCard.find(params[:question_card_id])
+    answer_cards = question_card.answer_cards
+    puts answer_cards
+    render json: answer_cards
   end
 
-  def create 
+  def create
     if session[:user_id]
       qcard = AnswerCard.create(answer_card_params)
+
       render json: qcard
     else
-      render json: {errors: "you're totally not logged in"}, :status => 403
+      error = ({Status: 'Error', Message: 'You are not logged in'})
+      render json: error, :status => 403
     end
   end
-  
+
   def show
-    if session[:user_id]
-      render json: AnswerCard.find(params[:id])
-    else
-      render json: {errors: "you're totally not logged in"}, :status => 403
-    end
+    render json: AnswerCard.find(params[:id])
   end
 
-  def update
-    card = AnswerCard.find(params[:id])
-    card.update(answer_card_params)
-    render json: card
-  end
-
-  def destroy
-    AnswerCard.find(params[:id]).destroy
-    head :no_content
-  end
+  # def update
+  #   card = AnswerCard.find(params[:id])
+  #   card.update(answer_card_params)
+  #   render json: card
+  # end
+  #
+  # def destroy
+  #   AnswerCard.find(params[:id]).destroy
+  #   head :no_content
+  # end
 
   def answer_card_params
     params.require(:answer_card).permit(:user_id, :answer_text, :question_card_id, :expiration_date)
   end
 
 end
-
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,31 @@
 # require 'tk'
 
 class UsersController < ApplicationController
-  def index
-  end
-
-  def show
-  end
 
   def create
+    # put this in front end
+    # pw = params["password"]
+    #
+    # if pw.length <= 7
+    #   @notice = "Password must be more than 7 characters! BlankU!"
+    #
+    # else
 
-    pw = params["password"]
-    
-    if pw.length <= 7
-      @notice = "Password must be more than 7 characters! BlankU!"
-      
+    if User.find_by(email: params['email'])
+        status = ({Status: 'Error', Message: 'A user with that email already exists'})
     else
-		stuff = User.create({
- 			fname: params["fname"],
- 			lname: params["lname"],
- 			email: params["email"],
- 			password: params["password"],
- 			image_url: params["image_url"]
- 			})
-		@user = stuff
-
-    	if @user.save
-      # Deliver the signup email
-      		UserNotifier.send_signup_email(@user).deliver
-      		redirect_to(@user, :notice => 'User created')
-    	else
-      		render :action => 'new'
-    	end
-
+      userNew = User.create({
+        fname: params["fname"],
+        lname: params["lname"],
+        email: params["email"],
+        password: params["password"],
+        image_url: params["image_url"]
+      })
+      UserNotifier.send_signup_email(userNew).deliver
     end
 
-	end
+    render json: status
+
+  end
 
 end
-
-
