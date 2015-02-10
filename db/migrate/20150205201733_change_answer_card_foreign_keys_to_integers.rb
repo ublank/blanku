c2626fb964c975471b908d885d185d32ed5e582c
cfmeyers <cfmeyers@gmail.com>
2015-02-10 07:57:17 -0500

    modified already created migration files to allow us to deploy to heroku on postgres.  I'm deeply, deeply sorry.

--- db/migrate/20150205201733_change_answer_card_foreign_keys_to_integers.rb
+++ db/migrate/20150205201733_change_answer_card_foreign_keys_to_integers.rb
@@ -1,6 +1,6 @@
 class ChangeAnswerCardForeignKeysToIntegers < ActiveRecord::Migration
   def change
-    change_column :answer_cards, :user_id, :integer
-    change_column :answer_cards, :question_card_id, :integer
+    change_column :answer_cards, :user_id, 'integer USING CAST(user_id AS integer)'
+    change_column :answer_cards, :question_card_id, 'integer USING CAST(question_card_id AS integer)'
   end
 end
--- db/migrate/20150205202005_change_question_card_foreign_key_to_integer.rb
+++ db/migrate/20150205202005_change_question_card_foreign_key_to_integer.rb
@@ -1,7 +1,7 @@
 class ChangeQuestionCardForeignKeyToInteger < ActiveRecord::Migration
 
   def change
-    change_column :question_cards, :user_id, :integer
+    change_column :question_cards, :user_id, 'integer USING CAST(user_id AS integer)'
   end
 
 end
